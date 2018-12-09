760d831c4ceab845b0f4bf768069928b7dd7348c
emma <hassling.emma@gmail.com>
2018-12-09 12:18:17 +0100

    Working with first eventlistener

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,6 +1,42 @@
-class QuizTime extends HTMLElement {
+
+const template = document.createElement('template')
+template.innerHTML = /* html */ `
+<style>
+  :host {
+    background: white;
+    font-size; 14px;
+    color: black;
+    width: 50%;
+    height: 300px;
+    float: center;
+  }
+</style>
+`
+
+class QuizTime extends window.HTMLElement {
   constructor () {
     super()
+
+    this.attachShadow({ mode: 'open' })
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  }
+  // start the quiz
+  startQuiz () {
+    let button = document.querySelector('#textbox button')
+    button.addEventListener('click', function clickListener () {
+      let nicknameValue = document.querySelector('#textbox input').value
+
+      if (nicknameValue.length > 0) {
+        let div = document.createElement('div')
+        let 
+      }
+        
+      }
+    }
+  }
+  async getQuestion () {
+    let question = await window.fetch('http://vhost3.lnu.se:20080/question/1')
+    return question.json()
   }
   /**  let req = new window.XMLHttpRequest()
   req.addEventListener('load', function () {
