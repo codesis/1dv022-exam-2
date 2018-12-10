c25e8ab6bb58f07114ba84be5177fbbe3da63111
emma <hassling.emma@gmail.com>
2018-12-10 20:30:51 +0100

    Adding some changes to css

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,18 +1,31 @@
 // template for shadow dom
 const template = document.createElement('template')
 template.innerHTML = /* html */ `
-<div id="quiz"></div>
+<div id="quiz">
 <div id="textbox">
     <input id="name" type="text" placeholder="Nickname"/>
     <button class="button">Start</button>
 </div>
+</div>
 
 <style>
   #textbox {
-    padding: 5px;
+    padding: 25px;
+  }
+  #name {
+    width: 100px;
+    height: 30px;
+    border-radius: 5px;
+    font-size: 20px;
   }
   .button {
     cursor: pointer;
+    background-color: #5d9598;
+    color: #ffe4e1;
+    font-size: 20px;
+    width: 100px;
+    height: 35px;
+    border-radius: 5px;
   }
 </style>
 `
@@ -27,35 +40,27 @@ class QuizTime extends window.HTMLElement {
     this.shadowRoot.appendChild(template.content.cloneNode(true))
     this._input = this.shadowRoot.querySelector('#name input')
     this._button = this.shadowRoot.querySelector('.button')
+    this.submitButton = document.querySelector('#choice')
   }
   // when the start button is clicked, do onClick
   connectedCallback () {
-    this._button.addEventListener('click', this._onClick)
+    this._button.addEventListener('click', this._onClickStart)
+    this.submitButton.addEventListener('click', this._onSubmit)
   }
   disconnectedCallback () {
-    this.removeEventListener('click', this._onClick)
+    this.removeEventListener('click', this._onClickStart)
   }
   // when clicking start, fetch first question
-  async _onClick () {
+  async _onClickStart () {
     this.obj = await window.fetch('http://vhost3.lnu.se:20080/question/1')
     this.obj = await this.obj.json()
     console.log(this.obj)
     // adding the question to the quiz
     document.getElementById('question').innerHTML = this.obj.question
   }
-  createQuestion () {
-  }
   // when submitting, client-data will be sent to the server
-  async _onSubmit () {
-    this.answer = await window.fetch('', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        answer: document.getElementById('#answer').value
-      })
-    })
+  _onSubmit () {
+
   }
 }
 
