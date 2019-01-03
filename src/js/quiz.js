c24310cd7834ada89f17967e56e0ee2f4807abb4
emma <hassling.emma@gmail.com>
2019-01-03 14:29:31 +0100

    Removing unnecessary code

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,37 +1,3 @@
-// import Timer from './timer.js'
-// template for shadow dom
-// const template = document.createElement('template')
-// template.innerHTML = /* html */ `
-// <div id="quiz">
-// <div id="textbox">
-// <form id="questionForm">
-//     <input id="name" type="text" placeholder="Nickname"/>
-//     <button class="button">Start</button>
-//     </form>
-// </div>
-// </div>
-
-// <style>
-//   #textbox {
-//     padding: 25px;
-//   }
-//   #name {
-//     width: 100px;
-//     height: 30px;
-//     border-radius: 5px;
-//     font-size: 20px;
-//   }
-//   .button {
-//     cursor: pointer;
-//     background-color: #5d9598;
-//     color: #ffe4e1;
-//     font-size: 20px;
-//     width: 100px;
-//     height: 35px;
-//     border-radius: 5px;
-//   }
-// </style>
-// `
 /**
  * @constructor
  * @param _input is for nickname and the questions requiring a written answer
@@ -41,8 +7,6 @@ class QuizTime extends window.HTMLElement {
   constructor () {
     super()
 
-    // this.attachShadow({ mode: 'open' })
-    // this.shadowRoot.appendChild(template.content.cloneNode(true))
     this._input = document.querySelector('#name input')
     this._button = document.querySelector('.button')
     this.submitButton = document.querySelector('#choice')
@@ -58,20 +22,6 @@ class QuizTime extends window.HTMLElement {
   disconnectedCallback () {
     this.removeEventListener('click', this._onClickStart)
   }
-  // making sure user has put 3 or more characters for a nickname
-  // nicknameText () {
-  //   let nickName = document.querySelector('#name')
-  //   let message = document.querySelector('#quiz p.nicknameText')
-
-  //   let nameText = nickName.value
-
-  //   if (nameText.length >= 3) {
-  //     // if 3 or more characters, fetch first question
-
-  //   } else {
-  //     message.innerHTML = 'You need to put in more characters to proceed'
-  //   }
-  // }
   // when clicking submit, fetch first question
   async _onClickSubmit () {
     let nickName = document.querySelector('#name')
@@ -89,18 +39,7 @@ class QuizTime extends window.HTMLElement {
     } else {
       message.innerHTML = 'You need to put in more characters to proceed'
     }
-    // this.obj = await window.fetch('http://vhost3.lnu.se:20080/question/1')
-    // this.obj = await this.obj.json()
-    // console.log(this.obj)
-    // // adding the question to the quiz
-    // document.getElementById('question').innerHTML = this.obj.question
   }
-  // when submitting, client-data will be sent to the server
-  // _onSubmit (event) {
-  //   if (event.type === 'click') {
-  //     event.preventDefault()
-  //   }
-  // }
 }
 
 window.customElements.define('quiz-time', QuizTime)
