67e3002ac6aa4483dff90ee2f4f5d41a8b962116
emma <hassling.emma@gmail.com>
2019-01-08 10:17:23 +0100

    Adding fetch for answers

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -8,8 +8,11 @@ class QuizTime extends window.HTMLElement {
   constructor () {
     super()
 
+    this.totalTime = 0
     this._input = document.querySelector('#name input')
     this._button = document.querySelector('#submit')
+    this._sendButton = document.getElementById('send')
+    this.nextURL = undefined
   }
   // when the start button is clicked, first do NOT refresh the page and then do onClick
   connectedCallback () {
@@ -18,13 +21,11 @@ class QuizTime extends window.HTMLElement {
     })
     // this._button.addEventListener('click', this.nicknameText)
     this._button.addEventListener('click', this._onClickStart)
+    this._sendButton.addEventListener('click', this.sendAnswer)
   }
   disconnectedCallback () {
     this.removeEventListener('click', this._onClickStart)
   }
-  async fetchQuestion () {
-  }
-
   // when clicking start, fetch first question if nickname value is correct
   async _onClickStart () {
     let nickName = document.querySelector('#name')
@@ -39,12 +40,31 @@ class QuizTime extends window.HTMLElement {
       console.log(this.obj)
       // adding the question to the quiz
       document.getElementById('question').innerHTML = this.obj.question
+      document.getElementById('quizbox-Start').style.visibility = 'hidden'
+      document.getElementById('quizbox-Answer').style.visibility = 'visible'
       this.nextURL = this.obj.nextURL
       document.getElementById('name').value = ''
       document.getElementsByClassName('nicknameText').innerHTML = ''
     } else {
       message.innerHTML = 'You need to put in a minimum of 3 characters to proceed'
     }
+  }
+  // function to send the users answer/chosen alternative to the server
+  async sendAnswer (e) {
+    e.preventDefault()
+
+    let answer = document.getElementById('answer').value
+    let alternative = document.getElementById('alternative').value
+
+    await window.fetch('http://vhost3.lnu.se:20080/answer/1', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ answer: answer, alternative: alternative })
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
   }
 }
 window.customElements.define('quiz-time', QuizTime)
