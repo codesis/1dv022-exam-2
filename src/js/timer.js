34d0fdc955d10ae42b0a4ff4754b531cbfde5691
emma <hassling.emma@gmail.com>
2019-01-10 00:06:24 +0100

    Fixing comments to timer module

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,3 +1,10 @@
+/**
+ * Timer class
+ * @constructor
+ * @param time {number} - countdown time
+ * @param elem {object} - element to which we print the timer
+ * @param owner {object} - object that created the timer
+ */
 class Timer {
   constructor (time, elem, owner) {
     this.time = time
@@ -6,20 +13,19 @@ class Timer {
     this.startTime = new Date().getTime()
     this.interval = undefined
   }
+  // starts the interval of the timer
   startTimer () {
     this.interval = setInterval(this.run.bind(this), 100)
   }
+  // function for each interval
   run () {
     let now = new Date().getTime()
-
     let diff = (now - this.startTime) / 1000
-
     let showTime = this.time - diff
 
     if (diff >= this.time) {
       showTime = 0
       clearInterval(this.interval)
-
       this.owner.gameOver('time')
     }
     if (showTime <= 10) {
@@ -28,12 +34,14 @@ class Timer {
       this.printTime(showTime.toFixed(0))
     }
   }
+  // function to stop the timer before it has reached 0
   stopTimer () {
     clearInterval(this.interval)
     let now = new Date().getTime()
 
     return (now - this.startTime) / 1000
   }
+  // function to print the timer to the element
   printTime (diff) {
     this.elem.replaceChild(document.createTextNode(diff), this.elem.firstChild)
   }
