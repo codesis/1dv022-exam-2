67cfe1356f3f429833f8dc481a1fcc8754701787
emma <hassling.emma@gmail.com>
2019-01-10 01:15:24 +0100

    Adding comments

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -85,22 +85,24 @@ class QuizTime extends window.HTMLElement {
       this.gameComplete()
     }
   }
+  // handler for when clicking on submit button or using enter (key)
   submit (e) {
     if (e.keyCode === 13 || e.which === 13 || e.type === 'click') {
       e.preventDefault()
-
+      // when this happens stop the timer
       this.totalTime += this.timer.stopTimer()
 
       let input
-
+      // remove our eventlistener so the user dont double submit
       this.form.removeEventListener('keypress', this.submit.bind(this))
       this.button.removeEventListener('click', this.submit.bind(this))
-
+      // checks wether the user has typed the answer or chosen one alt
       if (document.querySelector('#answer')) {
         input = document.querySelector('#answer').value
       } else {
         input = document.getElementsByName('alternative').checked.value
       }
+      // configuring the configuration for when we send the user's answer
       let config = {
         method: 'POST',
         url: this.nextURL,
@@ -120,13 +122,14 @@ class QuizTime extends window.HTMLElement {
     let template = document.querySelector('#gameEnds').content.cloneNode(true)
 
     let title
+    // if the cause is loss of time or else
     if (cause === 'time') {
       title = document.createTextNode('Uh oh, time is up! You lose!')
     } else {
       title = document.createTextNode('Wrong! You lose!')
     }
     template.querySelector('h1').appendChild(title)
-
+    // if there is already inputs on our highscore, show them
     if (hs.highscore.length > 0) {
       template.querySelector('h2').appendChild(document.createTextNode('Current highscore:'))
       let hsDoc = hs.highscoreDocument()
@@ -151,6 +154,18 @@ class QuizTime extends window.HTMLElement {
       div.insertBefore(newScore, div.firstChild)
     }
     this.clearDiv(document.querySelector('#content'))
+
+    let h3 = template.querySelector('.time')
+    let text = document.createTextNode(this.totalTime.toFixed(1))
+    h3.appendChild(text)
+
+    document.querySelector('#content').appendChild(template)
+  }
+  // clearing a div of choice
+  clearDiv (div) {
+    while (div.hasChildNodes()) {
+      div.removeChild(div.lastChild)
+    }
   }
 }
 window.customElements.define('quiz-time', QuizTime)
